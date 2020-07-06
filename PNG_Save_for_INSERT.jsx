cdfc4760eb6b68e9d0b0f520898f6f586ea6211e
AJ <47550996+DesignerAJ@users.noreply.github.com>
2020-07-06 10:37:42 +0900

    Update PNG_Save_for_INSERT.jsx

--- PNG_Save_for_INSERT.jsx
+++ PNG_Save_for_INSERT.jsx
@@ -1,22 +1,22 @@
-// 인서트 png 분리 저장 스크립트 by AJ (ver.ing)
-
+// 인서트 PNG 저장 스크립트 by AJ (ver.20190717)
 
 executeAction(app.stringIDToTypeID('save'), undefined, DialogModes.NO);
 
 var ingDoc = app.activeDocument;
 var ingDocName = ingDoc.name.split(".").shift();
 var ingDocPath = ingDoc.path;
-var background = ingDoc.layers[ingDoc.layers.length -1];
+var allLayers = ingDoc.layers
 
 var pngOptions = new PNGSaveOptions();
-pngOptions.compression = 0; // CC2019 PNG 저장 방법 바뀜...체크!
+pngOptions.compression = 0; // CC2019 PNG 저장 방법 바뀐듯...체크 필요!
 
 
 
 function cTID(s) { return app.charIDToTypeID(s); }
-function sTID(s) { return app.stringIDToTypeID(s); }
-function undo() { executeAction(cTID("undo", undefined, DialogModes.NO)); }
 
+function sTID(s) { return app.stringIDToTypeID(s); }
+
+function undo() { executeAction(cTID("undo", undefined, DialogModes.NO)); }
 
 function newGroupFromLayers(doc) {
     var desc = new ActionDescriptor();
@@ -28,7 +28,6 @@ function newGroupFromLayers(doc) {
     desc.putReference(cTID('From'), lref);
     executeAction(cTID('Mk  '), desc, DialogModes.NO);
 }
-
 
 function getSelectedLayers(doc) {
     var selLayers = [];
@@ -45,40 +44,32 @@ function getSelectedLayers(doc) {
     return selLayers;
 }
 
-
-function selectAllLayers() {
-    var ref = new ActionReference();
-    ref.putEnumerated( cTID('Lyr '), cTID('Ordn'), cTID('Trgt'));
-    var desc = new ActionDescriptor();
-    desc.putReference(cTID('null'), ref);
-    executeAction(sTID('selectAllLayers'), desc, DialogModes.NO);
-}
-
-
-function hideLayers() {
-    var ref = new ActionReference();
-    ref.putEnumerated(cTID('Lyr '), cTID('Ordn'), cTID('Trgt'));
-    var list = new ActionList();
-    list.putReference(ref);
-    var desc = new ActionDescriptor();
-    desc.putList(cTID('null'), list);
-    executeAction(cTID('Hd  '), desc, DialogModes.NO);
-
-    if (background.isBackgroundLayer) {
-        background.visible = false;
+function pngSavePerLayer() {
+    var selectedLayers = getSelectedLayers(ingDoc);
+    selectedLayers.reverse();
+    for (i = 0; i < selectedLayers.length; i++) {
+        if (selectedLayers[i].parent.typename == "LayerSet") {
+            alert("최상위 레이어, 그룹들을 선택하세요");
+            return;
+        }
     }
+
+    for (i=0; i < allLayers.length; i++){
+        allLayers[i].visible = false;
+    }
+
+    app.activeDocument.info.caption = "Saved by 인서트 PNG 저장 스크립트 v1.0";
+
+    for(i = 0; i < selectedLayers.length; i++) {
+        selectedLayers[i].visible = true;
+        ingDoc.saveAs(new File(ingDocPath + "/" + ingDocName + "_" + (i + 1)), pngOptions, true, Extension.LOWERCASE);
+        selectedLayers[i].visible = false;
+    }
+
+    executeAction(app.stringIDToTypeID('revert'), undefined, DialogModes.NO);
+
+    alert("레이어 " + selectedLayers.length + "개 PNG로 저장")
 }
 
 
-
-var selectedLayers = getSelectedLayers(ingDoc);
-selectAllLayers();
-hideLayers();
-
-for(i = 0; i < selectedLayers.length; i++) {
-    selectedLayers[i].visible = true;
-    ingDoc.saveAs(new File(ingDocPath + "/" + ingDocName + "_" + (i + 1)), pngOptions, true, Extension.LOWERCASE);
-    // selectedLayers[i].visible = false;
-}
-
-executeAction(app.stringIDToTypeID('revert'), undefined, DialogModes.NO);
+pngSavePerLayer();
